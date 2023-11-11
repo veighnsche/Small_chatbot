9a80bf205eb3996ecb493d2e32f5c2342038a81e
Vince Liem <vince.liem@iodigital.com>
2023-11-11 22:07:04 +0100

    going to redesign the content bubble

--- widgets/chat/src/Main.tsx
+++ widgets/chat/src/Main.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from "react";
+import OpenChatIcon from "./assets/chat-open.svg";
 import { NewChat } from "./components/buttons/NewChat/NewChat.tsx";
 import { Chat } from "./components/Chat/Chat.tsx";
 import { ChatHeader } from "./components/ChatHeader/ChatHeader.tsx";
 import { HistoryList } from "./components/HistoryList/HistoryList.tsx";
 import { Input } from "./components/Input/Input.tsx";
+import { IconButton } from "./components/utils/IconButton/IconButton.tsx";
 import "./Main.css";
 import "./reset.css";
 import { llamaEventBus } from "./services/llamaEventBus.ts";
 import { editLlamaChatParams } from "./slices/llamaChatParamsSlice.ts";
+import { toggleChatView } from "./slices/llamaChatViewSlice.ts";
 import { useLlamaDispatch, useLlamaSelector } from "./stores/llamaStore.ts";
 import { llamaSseAddMessage } from "./thunks/llamaSseAddMessage.ts";
-
 
 const Main = () => {
   const dispatch = useLlamaDispatch();
@@ -39,21 +41,36 @@ const Main = () => {
     };
   }, []);
 
-  const historyDrawerState = useLlamaSelector((state) => state.llamaChatView.isHistoryDrawerOpen);
-  const [renderDrawer, setRenderDrawer] = useState(historyDrawerState);
+  const isLarge = useLlamaSelector((state) => state.llamaChatView.isLarge);
+  const isHistoryDrawerOpen = useLlamaSelector((state) => state.llamaChatView.isHistoryDrawerOpen);
+  const [renderDrawer, setRenderDrawer] = useState(isHistoryDrawerOpen);
 
   useEffect(() => {
     const ANIMATION_DURATION = 300;
 
-    if (historyDrawerState) {
+    if (isHistoryDrawerOpen) {
       setRenderDrawer(true);
     } else {
       setTimeout(() => setRenderDrawer(false), ANIMATION_DURATION);
     }
-  }, [historyDrawerState]);
+  }, [isHistoryDrawerOpen]);
+
+  const historyClass = isHistoryDrawerOpen ? "" : "history-drawer-closed";
+  const largeClass = isLarge ? "large" : "";
+
+  const isOpen = useLlamaSelector((state) => state.llamaChatView.isOpen);
+  if (!isOpen) {
+    return (
+      <div className="open-chat-container">
+        <IconButton onClick={() => dispatch(toggleChatView())}>
+          <img width={32} height={32} src={OpenChatIcon} alt="Open Chat"/>
+        </IconButton>
+      </div>
+    );
+  }
 
   return (
-    <div className={`box-container ${historyDrawerState ? "" : "history-drawer-closed"}`}>
+    <div className={`box-container ${historyClass} ${largeClass}`}>
       {renderDrawer && <HistoryList/>}
       {renderDrawer && <NewChat/>}
       <ChatHeader/>
