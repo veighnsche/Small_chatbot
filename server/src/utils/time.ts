8fa4853b49d511dcf80fc5ac4e2bc3d1e4d841e8
Vince Liem <vince.liem@iodigital.com>
2023-12-02 13:52:21 +0100

    getting the timestamp now waits between 1 and 26 ms

--- server/src/utils/time.ts
+++ server/src/utils/time.ts
@@ -1,5 +1,7 @@
 export async function getTimeStamp(): Promise<string> {
-  await wait(1);
+  // random number between 1 and 26
+  const randomNumber = Math.floor(Math.random() * 26) + 1;
+  await wait(randomNumber);
   return Date.now().toString(36);
 }
 
