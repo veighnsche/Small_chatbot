5fb00c0caaafae735f04b0e5cd5febe40c6ca4b4
Vince Liem <vince.liem@iodigital.com>
2023-12-11 22:03:10 +0100

    Add LlamaFunction and LlamaFunctionCall to index export
    LlamaFunction and LlamaFunctionCall have been added to the list of exports in index.tsx. This extended the module's scope and allowed for these types to be accessed and reused in other parts of the application. A shorthand export for 'toValidAssistantString' function has also been added for ease of use.

--- npm_export/llama-tree-react/src/index.tsx
+++ npm_export/llama-tree-react/src/index.tsx
@@ -8,9 +8,12 @@ export {
   LlamaTreeProviderProps,
   LlamaLoadedSystemMessage,
   // LlamaFunctionsToFunctionCall,
+  LlamaFunction,
+  LlamaFunctionCall
 } from "./types/llamaTypes";
 export { useLlamaTree } from "./hooks/useLlamaTree";
 export { useLlamaFunction } from "./hooks/useLlamaFunction";
 export { useLlamaFunctionListener } from "./hooks/useLlamaFunctionListener";
 export { useLlamaFunctionStream } from "./hooks/useLlamaFunctionStream";
 export { toValidAssistantString } from "./utils/toValidAssistantString";
+export { toValidAssistantString as v } from "./utils/toValidAssistantString";
